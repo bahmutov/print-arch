43a634b2730ec4ba32a167c23c541627574bf780
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-10-18 13:26:59 -0400

    initial code

---
+++ src/print-arch-spec.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/* eslint-env mocha */
+const printArch = require('.')
+
+describe('print-arch', () => {
+  it('write this test', () => {
+    console.assert(printArch, 'should export something')
+  })
+})
